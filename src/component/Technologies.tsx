e1e1df2db8684851c61cae446a9dd1c02e98d4b6
NoyonAhmedJalal <noyonahmedjalal@gmail.com>
2026-09-13 17:58:13 +0600

    Updated

--- src/component/Technologies.tsx
+++ src/component/Technologies.tsx
@@ -1,8 +1,9 @@
 import React, { use, useState } from 'react';
 import type { Itechnologies } from './type/technologiesType';
 import { toast } from 'react-toastify';
+
 interface technologiesProps {
-  technologiesPromise: Promise<Itechnologies[]>
+  technologiesPromise: Promise<Itechnologies[]>;
 }
 
 const badgeColor: Record<string, string> = {
@@ -16,7 +17,6 @@ const badgeColor: Record<string, string> = {
 };
 
 const Technologies = ({ technologiesPromise }: technologiesProps) => {
-
   const techno = use(technologiesPromise);
 
   const [selectedStack, setSelectedStack] = useState<Itechnologies[]>([]);
@@ -32,107 +32,122 @@ const Technologies = ({ technologiesPromise }: technologiesProps) => {
     }
   };
 
+  // ১টি নির্দিষ্ট আইটেম মুছে ফেলার ফাংশন
+  const handleRemoveFromStack = (item: Itechnologies) => {
+    const updatedStack = selectedStack.filter((tech) => tech.id !== item.id);
+    setSelectedStack(updatedStack);
+    toast.error(`${item.name} removed from your stack.`);
+  };
 
+  // সব আইটেম একসাথে খালি করার ফাংশন
+  const handleRemoveAll = () => {
+    setSelectedStack([]);
+    toast.info('All technologies removed from stack.');
+  };
 
   return (
-    <section className='container mx-auto' >
-      <div className='mb-15'>
-        <h1 className='text-4xl font-bold'>Explore the
+    <section className="container mx-auto">
+      <div className="mb-15">
+        <h1 className="text-4xl font-bold">
+          Explore the{' '}
           <span className="bg-linear-to-r from-[#E12885] to-[#C832D0] bg-clip-text text-transparent">
             Technologies
-          </span> </h1>
-        <p className=''>Pick one technology per category to build your ideal stack.</p>
+          </span>
+        </h1>
+        <p className="">Pick one technology per category to build your ideal stack.</p>
       </div>
 
-      <div className='grid grid-cols-12 gap-20'>
+      <div className="grid grid-cols-12 gap-20">
+        <div className="col-span-9 grid grid-cols-3 gap-5">
+          {techno.map((items) => {
+            const isAdded = selectedStack.some((tech) => tech.id === items.id);
+            return (
+              <div key={items.id} className="card w-96 shadow-amber-200 border-none">
+                <figure className="flex justify-between p-4">
+                  <img className="h-15 mt-3" src={items.icon} alt={items.name} />
 
-        <div className='col-span-9 grid grid-cols-3 gap-5'>
-
-          {
-            techno.map((items) => {
-              const isAdded = selectedStack.some((tech) => tech.id === items.id);
-              return (
-                <div key={items.id} className="card  w-96 shadow-amber-200 border-none">
-
-                  <figure className="flex  justify-between p-4">
-                    <img className=" h-15 mt-3" src={items.icon} alt={items.name} />
-
-                    {items.badge ? (
-                      <span
-                        className={`px-3 py-1 text-xs font-semibold rounded-[5px] border-none h-fit ${badgeColor[items.badge] || 'bg-gray-100 text-gray-600 border-gray-200'
-                          }`} >
-                        {items.badge}
-                      </span>
-                    ) : (
-                      <div></div>
-                    )}
-
-                  </figure>
-                  <div className="card-body">
-                    <h2 className="card-title">{items.name}</h2>
-                    <p>{items.description}</p>
-                    <div className='flex items-center justify-between'>
-                      <p className='font-semibold'>{items.category}</p>
-                      <p className='font-semibold'>{items.difficulty}</p>
-                      <div className="flex items-center gap-1 font-bold text-gray-800">
-                        <span className="mask mask-star-2 bg-amber-400 w-4 h-4 inline-block"></span>
-                        <span>{items.rating}</span>
-                      </div>
-                    </div>
-                    <button
-                      onClick={() => handleAddToStack(items)}
-                      disabled={isAdded}
-                      className={`btn w-full ${isAdded ? 'btn-disabled' : 'btn-neutral'}`}
+                  {items.badge ? (
+                    <span
+                      className={`px-3 py-1 text-xs font-semibold rounded-[5px] border-none h-fit ${badgeColor[items.badge] || 'bg-gray-100 text-gray-600 border-gray-200'
+                        }`}
                     >
-                      {isAdded ? '✓ Added to Stack' : 'Add to Stack'}
-                    </button>
+                      {items.badge}
+                    </span>
+                  ) : (
+                    <div></div>
+                  )}
+                </figure>
+                <div className="card-body">
+                  <h2 className="card-title">{items.name}</h2>
+                  <p>{items.description}</p>
+                  <div className="flex items-center justify-between">
+                    <p className="font-semibold">{items.category}</p>
+                    <p className="font-semibold">{items.difficulty}</p>
+                    <div className="flex items-center gap-1 font-bold text-gray-800">
+                      <span className="mask mask-star-2 bg-amber-400 w-4 h-4 inline-block"></span>
+                      <span>{items.rating}</span>
+                    </div>
                   </div>
-                </div>
-              )
-            })
-          }
-
-        </div>
-
-
-
-        <div className='col-span-3'>
-  <div className="card bg-base-100 border border-gray-200 shadow-sm w-full sticky top-5 p-5 rounded-2xl">
-    <div className="card-body p-0">
-      <h2 className="card-title text-2xl font-bold text-gray-800">Your Stack</h2>
-      
-      
-      <p className='font-semibold text-gray-500 my-1 text-sm'>
-        {selectedStack.length === 0 
-          ? 'No technologies selected yet.' 
-          : `${selectedStack.length} Technology Selected`}
-      </p>
-
-      {selectedStack.length === 0 ? (
-        <div className="border-2 border-dashed border-gray-200 rounded-xl p-8 mt-3 text-center">
-          <p className='text-gray-400 font-medium'>Your stack is empty.</p>
-        </div>
-      ) : ( <div className="flex flex-col gap-3 mt-3">
-          {selectedStack.map((item) => (
-            <div key={item.id} className="flex items-center justify-between border border-gray-200 p-3 rounded-xl bg-white">
-              <div className="flex items-center gap-3">
-                <img className="w-7 h-7 object-contain" src={item.icon} alt={item.name} />
-                <div>
-                  <h4 className="font-bold text-sm text-gray-800">{item.name}</h4>
-                  <p className="text-[11px] text-gray-500">{item.category}</p>
+                  <button
+                    onClick={() => handleAddToStack(items)}
+                    disabled={isAdded}
+                    className={`btn w-full ${isAdded ? 'btn-disabled' : 'btn-neutral'}`}
+                  >
+                    {isAdded ? '✓ Added to Stack' : 'Add to Stack'}
+                  </button>
                 </div>
               </div>
-            </div>
-          ))}
+            );
+          })}
         </div>
-      )}
 
-    </div>
-  </div>
-</div>
+        <div className="col-span-3">
+          <div className="card bg-base-100 border border-gray-200 shadow-sm w-full sticky top-5 p-5 rounded-2xl">
+            <div className="card-body p-0">
+              <h2 className="card-title text-2xl font-bold text-gray-800">Your Stack</h2>
 
+              <p className="font-semibold text-gray-500 my-1 text-sm">
+                {selectedStack.length === 0
+                  ? 'No technologies selected yet.'
+                  : `${selectedStack.length} Technology Selected`}
+              </p>
+
+              {selectedStack.length === 0 ? (
+                <div className="border-2 border-dashed border-gray-200 rounded-xl p-8 mt-3 text-center">
+                  <p className="text-gray-400 font-medium">Your stack is empty.</p>
+                </div>
+              ) : (
+                <div className="flex flex-col gap-3 mt-3">
+                  {selectedStack.map((item) => (
+                    <div
+                      key={item.id}
+                      className="flex items-center justify-between border border-gray-200 p-3 rounded-xl bg-white"
+                    >
+                      <div className="flex items-center gap-3">
+                        <img className="w-7 h-7 object-contain" src={item.icon} alt={item.name} />
+                        <div>
+                          <h4 className="font-bold text-sm text-gray-800">{item.name}</h4>
+                          <p className="text-[11px] text-gray-500">{item.category}</p>
+                        </div>
+                      </div>
+
+                      <button onClick={() => handleRemoveFromStack(item)} className="btn btn-ghost btn-xs btn-circle text-gray-400 hover:text-error">
+                        ✕
+                      </button>
+                    </div>
+                  ))}
+
+                  <button
+                    onClick={handleRemoveAll}
+                    className="btn btn-outline btn-error w-full mt-2 rounded-xl">
+                    Remove All
+                  </button>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
       </div>
-
     </section>
   );
 };
