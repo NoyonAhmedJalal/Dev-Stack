131dc0de366c64fff394f81c14c6cb26e88153c7
NoyonAhmedJalal <noyonahmedjalal@gmail.com>
2026-09-13 18:01:12 +0600

    Updated

--- src/component/Technologies.tsx
+++ src/component/Technologies.tsx
@@ -39,7 +39,7 @@ const Technologies = ({ technologiesPromise }: technologiesProps) => {
     toast.error(`${item.name} removed from your stack.`);
   };
 
-  // সব আইটেম একসাথে খালি করার ফাংশন
+  // 
   const handleRemoveAll = () => {
     setSelectedStack([]);
     toast.info('All technologies removed from stack.');
